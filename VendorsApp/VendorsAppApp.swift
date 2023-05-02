600c3d4a7f7d6cf1aa12cab1fe8ec3664fdae4f1
Arthur Zavolovych <boredarthur@gmail.com>
2023-05-02 22:03:45 +0300

    initial commit

---
+++ VendorsApp/VendorsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VendorsAppApp.swift
+//  VendorsApp
+//
+//  Created by Артур Заволович on 02.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct VendorsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
